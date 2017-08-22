5837e96b43b15895138c0369ab60607f4e8f874b
mkhan <mkhan@allhomes.com.au>
2017-08-22 16:01:44 +1000

    info about crypt

--- src/rsa.h
+++ src/rsa.h
@@ -1,6 +1,10 @@
 //
 //  rsa.h
-//  Part of Mine library
+//  Part of Mine crypto library
+//
+//  You should not use this file, use include/mine.h
+//  instead which is automatically generated and includes this file
+//  This is seperated to aid the development
 //
 //  Copyright 2017 Muflihun Labs
 //
